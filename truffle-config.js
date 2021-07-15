b94385066cf53cf09512200ca5cc3c0153d3befa
Ben Sadeghi <ben.sadeghi1@gmail.com>
2021-07-15 13:33:34 +0800

    update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,33 @@
 const path = require("path");
+require("dotenv").config({path: "./.env"});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const AccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      port: 7545,
+      network_id: "*",
+      host: "127.0.0.1"
+    },
+    dev: {
+      port: 9545,
+      network_id: "*",
+      host: "127.0.0.1"
+    },
+    ganache_local: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", AccountIndex)
+      },
+      network_id: 5777
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.4"
     }
   }
 };
