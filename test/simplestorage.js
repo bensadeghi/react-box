e70557a698382e5acebf96869328dfede43ad0a9
Ben Sadeghi <ben.sadeghi1@gmail.com>
2021-07-15 13:37:21 +0800

    add setupchai.js

--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,5 +1,11 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
+const chai = require("./setupchai.js");
+const BN = web3.utils.BN;
+const expect = chai.expect;
+
+require("dotenv").config({path: "../.env"});
+
 contract("SimpleStorage", accounts => {
   it("...should store the value 89.", async () => {
     const simpleStorageInstance = await SimpleStorage.deployed();
